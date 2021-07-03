ef7b59a48933cdfe54bbcabea0ccb4f571839208
Khurshidul <khurshidulalam5@gmail.com>
2021-07-03 11:00:14 +0600

    updated all

--- sign_up.php
+++ sign_up.php
@@ -12,8 +12,13 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 
-    <title>Web Concept</title>
+    <title>create_new_account.com</title>
     <link rel="icon" type="image/jpg" href="image/teleios-icon.jpg">
+    <style>
+    h4{
+        color: #074566;
+    }
+    </style>
 </head>
 <body>
 
@@ -21,7 +26,7 @@
     <div class="container">
        <div class="row">
            <div class="col-md-12 mt-5 boarder boarder-primary">
-               <h1 class="text-center mb-5 boarder boarder-primary"><strong>Buyer's Form</strong></h1>
+               <h4 class="text-center mb-5 boarder boarder-primary"><strong>Fill up your Information.</strong></h4>
                <hr styel="height: 1px; color: black;  background-color:#2874A6;">
                <div class="row">
                <div class="col-md-5 mx-auto" style="background-color:#2ECC71 ;">
