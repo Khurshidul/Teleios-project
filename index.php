57442a001dd3eed54bfcec8f1de447618c5db653
Khurshidul <khurshidulalam5@gmail.com>
2021-07-03 06:39:27 +0600

    Renamed and updated

--- index.php
+++ index.php
@@ -1,191 +1,122 @@
 <?php
 
+$mysqli = new mysqli("localhost","khurshid","123456","members-data");
+
+// Check connection
+if ($mysqli -> connect_errno) {
+
+  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+  exit();
+}
+
+$user = isset($_POST['user'])?$_POST['user']:false;
+
+$password = isset($_POST['password'])?$_POST['password']:false;
+
+
+
+
+if($user != false && $password != false){
+
+
+$query = "INSERT INTO `userinfodata` (`user`, `password`)
+  VALUES ('$user', '$password')";
+
+ if ($mysqli->query($query) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $query . "<br>" . $mysqli->error;
+}
+
+}
+
+else{
+    echo "You don't submit your info";
+}
+
+
+
 
 ?>
 
 
+
 <!doctype html>
 <html lang="en">
-  <head>
+<head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" href="css/style.css">
 
- 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <title>Teleios.com</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
+    <title>Login.com</title>
     <link rel="icon" type="image/jpg" href="image/teleios-icon.jpg">
 
-  </head>
-  <body style= "background-color:#66FF99">
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#" style="color:#ADD8E6"><strong>Teleios Design & Market Field.</strong></a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="project.php">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Design.</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Services</a>
-        </li>  <li class="nav-item">
-          <a class="nav-link" href="about.php">About</a>
-        </li>  <li class="nav-item">
-          <a class="nav-link" href="#">Photos.</a>
-        </li>
+    <style>
+        .mx-auto{
+            padding-top: 200px;
+        }
+    </style>
+</head>
+<body style= "background-image: url('image/pic17.jpg');">
+<section>
+    <div class="container">
+       <div class="row">
+           <div class="col-md-6 mx-auto">
+
         
-      
-      </ul>
-      <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
+               <form action="project.php" method="post">
+                   <div class="row mb-3">
+                       <label for="user" class="col-sm-3 col-form-label" style= "color:#ADD8E6">@Username:</label>
+                       <div class="col-sm-9">
+                           <input
+                               type="text"
+                               class="form-control"
+                               placeholder="Username"
+                               id="user"
+                               name="user"
+                               value="">
+                       </div>
+                   </div>
+                   <div class="row mb-3">
+                       <label for="password" class="col-sm-3 col-form-label" style= "color:#ADD8E6">Password:</label>
+                       <div class="col-sm-9">
+                           <input
+                               type="password"
+                               class="form-control"
+                               placeholder="Password"
+                               id="password"
+                               name="password"
+                               value="">
+                       </div>
+                   </div>
+
+
+                   <button type="submit" class="btn btn-primary">Sign In</button>
+                   <a href="sign_up.php" class="btn btn-primary">Creat a new account.</a>
+
+               </form>
+              
+           </div>
+       </div>
+
     </div>
-  </div>
-</nav>
-<h1 style="color:#000099">
-  <strong>
-  Welcome to Teleios.
-</strong>
-
-</h1>
-
-
-
-<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="image/pic11.jpg" class="d-block w-100" alt="..." class="img-fluid">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>First slide label</h5>
-        <p>Some representative placeholder content for the first slide.</p>
-      </div>
-    </div>
-    <div class="carousel-item">
-      <img src="image/pic12.jpg" class="d-block w-100" alt="..." class="img-fluid">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Second slide label</h5>
-        <p>Some representative placeholder content for the second slide.</p>
-      </div>
-    </div>
-    <div class="carousel-item">
-      <img src="image/pic13.jpg" class="d-block w-100" alt="..." class="img-fluid">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Third slide label</h5>
-        <p>Some representative placeholder content for the third slide.</p>
-      </div>  
-    </div>
-    
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
-
-<section class = "my-5">
-<div class= "py-5">
-<h3 class= "text-center bg-gray"><u>About Us.</u></h3>
-</div>
-<div class= "container-fluid">
-<div class= "row">
-     <div class= "col-lg-6 col-mid-6 col-12" style="boarder">
-<img src="image/pic16.jpg" class="img-fluid img-thumbnail">
-     </div>
-     <div class= "col-lg-6 col-mid-6 col-12 text-center py-5" style="boarder">
-     <h2 class= "display-4"><u>Teleios Fasion.</u></h2>
-     <p class= "py-5">
-  Here You can Choose any design what you need.You can also submit your own design to make sure your order.
-If you need any unique design you can try our field. There are many expart designer who can design any garments
-which you need. 
-     </p>
-     <a href="about.php" style="color:black" class="btn btn-success">Check More.</a>
-     </div>
-     <div class= "col-lg-6 col-mid-6 col-12 py-5 w-50 text-center" style="boarder" >
-     <h2 class= "display-4"><u>Teleios Fasion.</u></h2>
-     <p class= "py-5">
-  Here You can Choose any design what you need.You can also submit your own design to make sure your order.
-If you need any unique design you can try our field. There are many expart designer who can design any garments
-which you need. 
-     </p>
-     <a href="about.php" style="color:black" class="btn btn-success">Check More.</a>
-     </div>
-     <div class= "col-lg-6 col-mid-6 col-12 w-50" style="boarder">
-<img src="image/pic10.jpg" class=" img-fluid img-thumbnail">
-
-  
-     </div>
-</div>
-</div>
 </section>
 
-<section class = "my-5">
-<div class= "py-5">
-<h3 class= "text-center bg-gray"><u>Our Designs.</u></h3>
-</div>
+<!-- Optional JavaScript; choose one of the two! -->
 
-<div class= "container-fluid">
-  <div class= " row">
-  <div class= "col-lg-4 col-mid-4 col-12">
-  <div class="card" style="width:400px">
-  <img class="card-img-top img-thumbnail" src="image/pic1.jpg" alt="Card image">
-  <div class="card-body py-4">
-    <h4 class="card-title">Design No:01</h4>
-    <p class="card-text">If You like this make sure your confirmation.</p>
-    <a href="#" class="btn btn-primary">See more details.</a>
-  </div>
-</div>
-</div>
-<div class= "col-lg-4 col-mid-4 col-12">
-  <div class="card" style="width:400px">
-  <img class="card-img-top img-thumbnail" src="image/pic2.jpg" alt="Card image">
-  <div class="card-body py-4">
-    <h4 class="card-title">Design No:02</h4>
-    <p class="card-text">If You like this make sure your confirmation.</p>
-    <a href="#" class="btn btn-primary">See more details.</a>
-  </div>
-</div>
-</div>
-<div class= "col-lg-4 col-mid-4 col-12">
-  <div class="card" style="width:400px">
-  <img class="card-img-top img-thumbnail" src="image/pic7.jpg" alt="Card image">
-  <div class="card-body py-4">
-    <h4 class="card-title">Design No:03</h4>
-    <p class="card-text">If You like this make sure your confirmation.</p>
-    <a href="#" class="btn btn-primary">See more details.</a>
-  </div>
-</div>
-</div>
- 
-</div>
+<!-- Option 1: Bootstrap Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
 
-</div>
-
-</section>
-
-
-
-    
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-    
-  </body>
+<!-- Option 2: Separate Popper and Bootstrap JS -->
+<!--
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
+-->
+</body>
 </html>
+
